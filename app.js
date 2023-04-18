54fbbac0dd64eca913cd1ad1967f841acf7bbef3
C-junying <doufukeai@163.com>
2023-04-18 22:53:03 +0800

    后端配置文件

--- app.js
+++ app.js
@@ -1,41 +1,51 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+const bodyParser = require('body-parser')
+const expressjwt = require('express-jwt')
+const cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// 自定义模块
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
 
-var app = express();
+var app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(logger('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+//将静态资源托管，这样才能在浏览器上直接访问预览图片或则html页面
+app.use(express.static(path.join(__dirname, './public')))
+// 跨域
+app.use(cors())
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//token验证 - 需要在所有路由前添加，否则直接进路由不进验证
+// const secretKey = 'junying'
+// app.use(expressjwt({
+//   secret: secretKey,  // 签名的密钥 或 PublicKey => req.user
+//   algorithms: ['HS256']
+// }).unless({
+//   path: [ /^\/images\//]  // 指定路径不经过 Token 解析
+// }))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// 路由前缀地址
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+//404 错误
+app.use(function (req, res, next) {
+  res.status(404).json({ code: 404, msg: '页面不存在！' })
+})
+// 其他错误处理
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ code: 401, msg: err.message })
+  } else {
+    res.status(err.status || 500).json({ code: err.status || 500, msg: err.message })
+  }
+})
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+module.exports = app
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// 注册新用户
+router.post('/register', (req, res) => {
+  res.send('register')
+})
+// 登录
+router.get('/login', (req, res) => {
+  res.send('login')
+})
 
-module.exports = router;
+module.exports = router
